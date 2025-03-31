d7bd53f5f710d984dfcbfd77809b88623ec0f773
Jherald Vibar <vibar_jherald@spcc.edu.ph>
2025-03-31 22:45:59 +0800

    Added Add Task

---
+++ Controllers/Auth/AuthController.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\Account;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    public function register() {
+        return view("Auth.register");
+    }
+
+    public function store(Request $request) {
+
+        $validated = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password'=> 'required|min:8|regex:/[0-9]/',
+        ]);
+
+        if($validated->fails()) {
+            return redirect()->back()->withErrors($validated)->withInput();
+        }
+
+        $user = User::create([
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'password'=> bcrypt($request->password),
+        ]);
+
+        return redirect()->route('loginForm')->with('success', "Registration Success!");
+    }
+
+    public function redirect() {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function googleAuth() {
+        try {
+            $google_user = Socialite::driver('google')->user();
+
+
+            $user = User::where('email', $google_user->getEmail())->first();
+
+            if ($user) {
+
+                if (!$user->google_id) {
+                    $user->update([
+                        'google_id' => $google_user->getId(),
+                    ]);
+                }
+            } else {
+
+                $user = User::create([
+                    'name' => $google_user->getName(),
+                    'email' => $google_user->getEmail(),
+                    'google_id' => $google_user->getId(),
+                    'image' => $google_user->getAvatar(),
+                ]);
+            }
+
+            Auth::login($user);
+            $accountExist = Account::where('user_id', $user->id)->first();
+            if($accountExist) {
+                return redirect()->route('user-today');
+            }
+            return redirect()->route('createForm', ['id' => $user->id]);
+
+        } catch (\Throwable $e) {
+            return redirect()->route('registrationForm')->with('error', 'Something went wrong! ' . $e->getMessage());
+        }
+    }
+
+    public function login() {
+        return view('Auth.login');
+    }
+
+    public function authenticate(Request $request) {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if (!$user) {
+            return redirect()->back()->withErrors(['email' => 'Email not found.'])->withInput();
+        }
+
+        if (!Auth::attempt($validated)) {
+            return redirect()->back()->withErrors(['password' => 'Invalid credentials.'])->withInput();
+        }
+        $request->session()->regenerate();
+        $user = Auth::user();
+        $accountExist = Account::where('user_id', $user->id)->first();
+        if($accountExist) {
+            return redirect()->route('user-today');
+        }
+        return redirect()->route('createForm', ['id' => $user->id]);
+    }
+
+    public function logout(Request $request) {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('loginForm')->with('success', 'Logout Success!');
+    }
+
+    public function forgotPassword() {
+        return view('Auth.forget-password');
+    }
+
+    public function forgotPasswordPost(Request $request) {
+        $request->validate([
+            'email' => 'required|email|exists:users'
+        ]);
+
+        $token = Str::random(64);
+
+        DB::table('password_reset_tokens')->insert([
+            'email' => $request->email,
+            'token' => $token,
+            'created_at' => Carbon::now(),
+        ]);
+
+        Mail::send('emails.forgot-password', ['token' => $token], function($message) use ($request) {
+            $message->to($request->email);
+            $message->subject("Reset Password");
+        });
+
+        return redirect()->route('forgotpassForm')->with('success', "Sending Email To Reset Password Success!");
+    }
+
+    public function resetPasswordForm($token) {
+        return view("Auth.new-password", compact('token'));
+    }
+
+    public function resetPassword(Request $request) {
+        $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required|string|min:6',
+            'password_confirmation' => 'required',
+        ]);
+
+        $updatePassword = DB::table('password_reset_tokens')->where([
+            'email' => $request->email,
+            'token' => $request->token,
+        ])->first();
+
+        if(!$updatePassword) {
+            return redirect()->route('reset')->with('error', "Password Reset Error!")->withInput();
+        }
+
+        User::where("email", $request->email)->update(["password" => Hash::make($request->password)]);
+
+        DB::table('password_reset_tokens')->where(["email" => $request->email])->delete();
+
+        return redirect()->route('loginForm')->with('success', 'Password reset success!');
+    }
+
+
+}
---
+++ Controllers/User/UserController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Account;
+use App\Models\TaskModel;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function todayPage() {
+       $user = Auth::user();
+       $account = Account::where('user_id', $user->id)->first();
+       $tasks = TaskModel::where('user_id', $user->id)->get();
+       if(!$account) {
+         return redirect()->route('createForm', ['id' => $user->id])->with('error', 'You need to Create an Account!');
+       }
+       return view('Users.today', compact('account', 'tasks'));
+    }
+
+    public function createForm($id) {
+        $user = User::find($id);
+        return view('Users.create_acc', compact('user'));
+    }
+
+    public function storeAccount(Request $request, $id) {
+        $user = User::findOrFail($id);
+        $validator = Validator::make($request->all(), [
+            'username' => 'required',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        if($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $validated = $validator->validated();
+
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $fileName = time() . '.' . $file->getClientOriginalExtension();
+
+
+            $file->move(public_path('profile-pic'), $fileName);
+
+
+            $validated['image'] = $fileName;
+        }
+
+        Account::create([
+            'username' => $request->username,
+            'image' => $validated['image'] ?? null,
+            'user_id' => $user->id,
+        ]);
+        return redirect()->route('user-today')->with('success', 'Successfully Created an Account');
+    }
+
+}
