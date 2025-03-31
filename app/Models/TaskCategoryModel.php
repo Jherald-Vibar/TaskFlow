523a016ada83d4e0ac9e0eba1f862db6c5498b7d
Jherald Vibar <vibar_jherald@spcc.edu.ph>
2025-03-31 23:26:01 +0800

    Add Added Task

---
+++ app/Models/TaskCategoryModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Console\View\Components\Task;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TaskCategoryModel extends Model
+{
+    use HasFactory;
+    protected $table = 'task_categories';
+
+    protected $fillable = ['user_id', 'category_name'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, 'category_id');
+    }
+}
---
+++ app/Models/TaskModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TaskModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tasks';
+
+    protected $fillable = ['user_id', 'task_name', 'description', 'category_id', 'priority', 'due_date'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(TaskCategoryModel::class, 'category_id');
+    }
+}
