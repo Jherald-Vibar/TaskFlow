9e6aeca24d07c6a9729bf06250f2831a3a783fda
Jherald Vibar <vibar_jherald@spcc.edu.ph>
2025-03-31 23:27:00 +0800

    Add Added Task

---
+++ database/migrations/2025_03_30_171558_create_task_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->nullable()->constrained('task_categories')->onDelete('cascade');
+            $table->string('task_name');
+            $table->text('description')->nullable();
+            $table->enum('priority', ['Low', 'Medium', 'High']);
+            $table->date('due_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
